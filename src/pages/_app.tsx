64264a97506da8058b28330875e855ee8167e401
Colin Richards <26richco@gmail.com>
2025-07-07 12:03:37 -0600

    Googlel Tag Script added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
-import "@/styles/globals.css";
+import { AppProps } from 'next/app'
+import '@/styles/globals.css'
 import '@/i18n/i18n'
-import type { AppProps } from "next/app";
-import { Analytics } from '@vercel/analytics/react' // <-- ✅ import it here
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Google Tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17298586607"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17298586607');
+        `}
+      </Script>
+
       <Component {...pageProps} />
-      <Analytics /> {/* <-- ✅ include it after your component */}
+      <Analytics />
     </>
-  );
+  )
 }

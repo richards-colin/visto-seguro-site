f6a2500675f4214d4f53243306c6248c2c870e29
Colin Richards <26richco@gmail.com>
2025-07-29 12:40:57 -0600

    GTM error fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from 'next/app'
 import '@/styles/globals.css'
 import '@/i18n/i18n'
 import { Analytics } from '@vercel/analytics/react'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

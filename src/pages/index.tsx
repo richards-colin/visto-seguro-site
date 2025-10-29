d0270c8ae9d2bd52449001a87a376b7f5c142c81
Colin Richards <26richco@gmail.com>
2025-10-29 12:54:16 -0600

    Hero image updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,27 +12,6 @@ export default function Home() {
       <Navbar />
       <Hero imageUrl="/arches.jpg" />
       <HowWeHelp/>
-            
-      <Hero imageUrl="/archesGrey.jpg" />
-      <HowWeHelp/>
-            
-      <Hero imageUrl="/fieldMountains.jpg" />
-      <HowWeHelp/>
-            
-      <Hero imageUrl="/redMountain.jpg" />
-      <HowWeHelp/>
-            
-      <Hero imageUrl="/singleMountain.jpg" />
-      <HowWeHelp/>
-            
-      <Hero imageUrl="/sky.jpg" />
-      <HowWeHelp/>
-            
-      <Hero imageUrl="/yellowArch.jpg" />
-      <HowWeHelp/>
-                  
-      <Hero imageUrl="/yellowMountain.jpg" />
-      <HowWeHelp/>
       <Values />
       <Footer />
     </main>
